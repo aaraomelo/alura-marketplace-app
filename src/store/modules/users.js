2a980e5af28a91540068cbb777b15d620f1e7493
Aarão Melo Lopes <aarao.melo.lopes@gmail.com>
2020-04-17 20:53:51 -0300

    registra um novo usuário

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,23 +1,40 @@
 
 const state = {
-
-};
-
-const getters = { 
-
-};
-
-const actions = {
-  
+    newUser: {
+        name: 'test',
+        email: 'teste@teste.com',
+        password: '123456'
+    },
+    user: {},
+    token: ''    
 };
 
 const mutations = {
+    setNewUser: (state, { newUser }) => state.newUser = newUser,
+    setUser: (state, { user }) => state.user = user,
+    setToken: (state, { token }) => state.token = token
+};
 
+const getters = {
+    getNewUser: (state) => state.newUser,
+    getUser: (state) => state.user,
+    getToken: (state) => state.token
+};
+
+const actions = {
+    async registerNewUser({ state, commit, dispatch }) {
+        const res = await dispatch('POST', {
+            uri: 'users',
+            data: state.newUser
+        })
+        commit('setUser', { user: res.user });
+        commit('setToken', { token: res.token });
+    }
 };
 
 export default {
     state,
+    mutations,
     getters,
-    actions,
-    mutations
+    actions
 }
